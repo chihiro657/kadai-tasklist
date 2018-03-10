7d43adb01a06ee4b24a68c3a9e22e9c809ff135d
EC2 Default User <ec2-user@ip-172-31-5-37.us-east-2.compute.internal>
2018-03-10 11:19:57 +0000

    CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,21 @@
 class TasksController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
-  before_action :set_task, only: [:edit,:show,:update,:destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:edit, :show, :update, :destroy]
+  before_action :set_task, only: [:edit,:update,:destroy]
+  
+  include SessionsHelper
   
   def index
-    @tasks = Task.all.page(params[:page]).per(5)
+     @tasks = current_user.tasks.all.page(params[:page]).per(5)
   end
   
   def create
-    @task = Task.new(task_params)
-    
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = "タスクは正常に投稿されました"
-      redirect_to @task
+      redirect_to root_url
     else
+      @tasks = current_user.tasks.order("created_at DESC").page(params[:page])
       flash.now[:danger] = "タスクは投稿されませんでした"
       render :new
     end
@@ -26,6 +29,7 @@ class TasksController < ApplicationController
   end
 
   def show
+    @task = current_user.tasks.find(params[:id])
   end
 
   def update
@@ -44,14 +48,21 @@ class TasksController < ApplicationController
     flash[:success] = "タスクは正常に削除されました"
     redirect_to tasks_url
   end
-end
+  
+  private
 
-private
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
-def set_task
-  @task = Task.find(params[:id])
-end
+  def task_params
+    params.require(:task).permit(:content,:status)
+  end
 
-def task_params
-  params.require(:task).permit(:content,:status)
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
